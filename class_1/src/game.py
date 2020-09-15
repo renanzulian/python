eb9d3547489c19127395120384d506a940a59587
Renan Zulian <renanzulian@gmail.com>
2020-09-14 22:18:54 -0300

    While true

--- class_1/src/game.py
+++ class_1/src/game.py
@@ -4,12 +4,14 @@ print("************************")
 
 secret_number = 12
 
-number = int(input("Input your number: "))
-print("Your number is", number)
+while True:
+    number = int(input("Input your number: "))
+    print("Your number is", number)
 
-if number == secret_number:
-    print("Congrats you are right")
-elif number > secret_number:
-    print("The secret number is biggest")
-else:
-    print("The secret number is smallest")
+    if number == secret_number:
+        print("Congrats you are right")
+        break
+    elif number > secret_number:
+        print("The secret number is biggest")
+    else:
+        print("The secret number is smallest")
