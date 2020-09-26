57fded7e861a19333f8caa6d4391e6383cdb9c78
Renan Zulian <renanzulian@gmail.com>
2020-09-26 18:41:32 -0300

    feat: private proeperties in oython

---
+++ class_3/models/account.py
@@ -0,0 +1,12 @@
+
+class Account:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
